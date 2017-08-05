cb1bc15312cce30bb2726e13da6440a2ed0fcf68
miya0001 <miya@wpist.me>
2017-08-06 00:46:30 +0900

    fix problem for compatible

--- tests/test-custom-field.php
+++ tests/test-custom-field.php
@@ -8,7 +8,7 @@ class Custom_Field_Tests extends WP_UnitTestCase {
 	function test_class() {
 		$test01 = new Test01( 'hello', 'Hello' );
 		$test01->add( 'post' );
-		$this->assertSame( 'post', $test01->admin_enqueue_scripts() );
+		$this->assertSame( 'post', $test01->admin_enqueue_scripts( 'hello' ) );
 		$this->assertSame( 'post', $test01->meta_box_callback( array() ) );
 
 		ob_start();
@@ -21,7 +21,7 @@ class Custom_Field_Tests extends WP_UnitTestCase {
 
 class Test01 extends \Miya\WP\Custom_Field
 {
-	public function admin_enqueue_scripts()
+	public function admin_enqueue_scripts( $hook )
 	{
 		return $this->post_type;
 	}
